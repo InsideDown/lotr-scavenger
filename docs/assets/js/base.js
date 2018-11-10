9d91f405129bfce0622a16495f44d111f28d747d
Paul Mealy <Paul.Mealy@wearepop.com>
2018-11-10 12:58:27 -0500

    - updating hint pages

--- docs/assets/js/base.js
+++ docs/assets/js/base.js
@@ -56,7 +56,7 @@ Object.defineProperty(exports,"__esModule",{value:true});var _Events=__webpack_r
 
 // removed by extract-text-webpack-plugin
     if(false) {
-      // 1541811078168
+      // 1541872701448
       var cssReload = require("../../node_modules/css-hot-loader/hotModuleReplacement.js")(module.id, {"fileMap":"{fileName}"});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
--- docs/assets/js/home.js
+++ docs/assets/js/home.js
@@ -40,7 +40,7 @@ var ScavengerHunt=function(){function ScavengerHunt(){_classCallCheck(this,Scave
 
 // removed by extract-text-webpack-plugin
     if(false) {
-      // 1541811078167
+      // 1541872701447
       var cssReload = require("../../node_modules/css-hot-loader/hotModuleReplacement.js")(module.id, {"fileMap":"{fileName}"});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
